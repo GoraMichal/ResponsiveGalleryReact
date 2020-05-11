6e50347cfbbf6492f398f07b0e6f016d39d0511b
Michał Góra <michalgorazs3@gmail.com>
2020-05-11 21:13:09 +0200

    ReactGallery

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+//view library
+import React from 'react'; 
+//what screen to rendering
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <h1>Witam</h1>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
