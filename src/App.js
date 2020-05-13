5278abd08466624d0f84073197e6cc161426e4f4
Michał Góra <michalgorazs3@gmail.com>
2020-05-13 18:08:14 +0200

    componentDidMount css

--- src/App.js
+++ src/App.js
@@ -5,19 +5,35 @@ import SearchBox from './SearchBox';
 //Destrukturyzacja
 import { images } from './Gallery.js';
 
+import './index.css';
+
 //Klasa App zawiera konstruktor do tworzenia dwoch stanow: images, searchfield
 //Stany sluza do renderowania komponentow 
 //App wyszukuje zmian w SearchBox za pomoca onSearchChange
 //App komunikuje sie z CardList w celu przefiltrowania wynikow wyszukwiania
+//https://reactjs.org/docs/handling-events.html
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
             image: images,
+            //image: [],
             searchField: ''
         }
     }
+
+    //Aktualizacja stanu komponentu po aktualizacji DOM (cykl zycia komponentu)
+    //componentDidMount() {
+    //    fetch('https://jsonplaceholder.typicode.com/users')
+    //        .then(response => {
+    //            return response.json();
+    //        })
+    //        .then(users => {
+    //            this.setState({ image: users })
+    //        });
+    //    //console.log('check');
+    //}
 
     onSearchChange = (event) => {
         this.setState({ searchField: event.target.value })
@@ -27,13 +43,18 @@ class App extends Component {
         const filteredImages = this.state.image.filter(image => {
             return image.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        return (
-            <div className='tc' >
-                <h1>Art Gallery</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList images={filteredImages} />
-            </div>
-        );
+        if (this.state.image.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+
+            return (
+                <div className='tc'>
+                    <div className='galleryTitle'>Art Gallery</div>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList images={filteredImages} />
+                </div>
+            );
+        }
     }
 }
 
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './card.css';
 
 //destructuring - ({name, author, id})
 const Card = ({ nameCard, authorCard, idCard }) => {
 	return (
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+		//tachyon
+		<div className='tc bg-washed-red dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<img alt='image' src={`https://www.galeriaperspektywa.pl/images/${idCard}`} />
 			<div>
 				<h2>{nameCard}</h2>
